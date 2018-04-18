28dd208dbaa2a808aa66f81a43941496aa3985a9
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-04-18 14:15:05 +0200

    chomp

---
+++ lib/chomp.rb
@@ -0,0 +1,13 @@
+def chomp(string)
+    i = 0
+    output = ""
+    if string[-1] == "\n"
+        while i < string.size
+            output += string[i]
+            i += 1
+        end
+        return string
+    else
+        return string
+    end        
+end
