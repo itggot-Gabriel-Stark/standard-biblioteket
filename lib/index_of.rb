0f9cb75f3a71eb3e5fdf25b96c4f9e271a62912a
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-04-25 14:43:12 +0200

    index of

---
+++ lib/index_of.rb
@@ -0,0 +1,27 @@
+def index_of(string,letters)
+    i = 0
+    contains = false
+    index_start = nil
+    while i < string.size
+        j = 0
+        if string[i] == letters[j]
+            while j < letters.size
+                if string[i + j] == letters[j]
+                    contains = true
+                else
+                    contains = false
+                    j = letters.size
+                end
+                j += 1
+            end
+            if contains == true
+                index_start = i
+                i = string.size
+            end
+        end
+        i += 1
+    end
+    return index_start
+end
+
+p index_of("foobar","baz")
