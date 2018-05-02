6d73c77ce73a238f2d95f5023fb77a8b3c95b6dd
itggot-Gabriel-Stark <gabriel.stark@itggot.se>
2018-05-02 13:29:08 +0200

    quick sort

--- dev/quick_sort.rb
+++
@@ -1,4 +0,0 @@
-def quick_sort(unsorted)
-    pivot = unsorted[rand(unsorted.size)]
-    i = 0
-end
---
+++ lib/quick_sort.rb
@@ -0,0 +1,51 @@
+require 'Benchmark'
+def list(arr)
+    i = 0
+    sorted = []
+    sorted = quick_sort(arr)
+    while sorted.size < arr.size
+        if sorted[i].size > 1   
+            checked = quick_sort(sorted[i])
+            j = 0
+            k = i
+            while j < checked.size
+                sorted.insert(k,checked[j])
+                j += 1
+                k += 1
+            end
+            sorted.delete_at(k)
+        else
+            i += 1
+        end
+    end
+    return sorted.flatten
+end
+
+def quick_sort(arr)
+    i = 0
+    pivot_index = arr.size/2
+    lower_than_pivot = []
+    higher_than_pivot = []
+    pivot = arr[pivot_index]
+    while i < arr.size
+        if i == pivot_index
+            i += 1
+        else
+            if arr[i] < pivot
+                lower_than_pivot << arr[i]
+            else
+                higher_than_pivot << arr[i]
+            end
+            i += 1
+        end
+    end
+    sorted = []
+    if lower_than_pivot.size > 0
+        sorted << lower_than_pivot
+    end   
+    sorted << [pivot]
+    if higher_than_pivot.size > 0
+        sorted << higher_than_pivot
+    end
+    return sorted
+end
